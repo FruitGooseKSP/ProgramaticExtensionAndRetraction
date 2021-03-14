9ef055314fb0be080b425664c445e364a8e9e49c
FruitGooseKSP <jrf091981@gmail.com>
2021-03-14 04:01:13 +0000

    revised code for power toggle

--- ProgramaticExtensionAndRetraction/PEAR.cs
+++ ProgramaticExtensionAndRetraction/PEAR.cs
@@ -8,6 +8,25 @@ namespace ProgramaticExtensionAndRetraction
 {
     public class PEAR : PartModule
     {
+        [KSPEvent(active = true, guiActive = true, guiActiveEditor = true, isPersistent = true, guiName = "Toggle PEAR Status")]
+        public void TogglePearStatus()
+        {
+            if (isActive)
+            {
+                pearStatus = "OFFLINE";
+                isActive = false;
+                this.part.GetComponent<PEAR>().Events["ExtendAll"].active = false;
+                this.part.GetComponent<PEAR>().Events["RetractAll"].active = false;
+            }
+            else if (!isActive)
+            {
+                pearStatus = "Active";
+                isActive = true;
+                CheckState();
+            }
+        }
+
+
         // Event to extend all
 
         [KSPEvent(active = true, guiActive = true, isPersistent = true, guiName = "Extend ALL Extendables")]
@@ -21,23 +40,35 @@ namespace ProgramaticExtensionAndRetraction
                 }
 
                 if (gTG)
-
-                    foreach (var part in deployableList)
+                {
+                    foreach (var part in FlightGlobals.ActiveVessel.Parts)
                     {
-                        part.SendEvent("Extend");
-                        part.GetComponent<PEAR>().Events["ExtendAll"].active = false;
-                        part.GetComponent<PEAR>().Events["RetractAll"].active = true;
+                        if (part.HasModuleImplementing<PEAR>())
+                        {
+
+
+                            if (part.GetComponent<PEAR>().pearStatus == "Active")
+                            {
+                                part.SendEvent("Extend");
+                                part.GetComponent<PEAR>().Events["ExtendAll"].active = false;
+                                part.GetComponent<PEAR>().Events["RetractAll"].active = true;
+                            }
+                            else continue;
+                        }
                     }
+                }
                 else
                 {
                     ScreenMessage screenMessage = new ScreenMessage("PEAR is disabled - deploy fairings first",
                         3.0f, ScreenMessageStyle.KERBAL_EVA);
                     ScreenMessages.PostScreenMessage(screenMessage);
                 }
+
             }
+
             catch
             {
-                Debug.LogError("Error Ref- KSPEvent: ExtendAll; caught exception");
+                Debug.LogError("Error Ref- PEAR: KSPEvent: ExtendAll; caught exception");
             }
         }
 
@@ -46,20 +77,37 @@ namespace ProgramaticExtensionAndRetraction
         [KSPEvent(active = true, guiActive = true, isPersistent = true, guiName = "Retract ALL Extendables")]
         public void RetractAll()
         {
-            foreach (var part in deployableList)
+            foreach (var part in FlightGlobals.ActiveVessel.Parts)
             {
-                part.SendEvent("Retract");
-                part.GetComponent<PEAR>().Events["ExtendAll"].active = true;
-                part.GetComponent<PEAR>().Events["RetractAll"].active = false;
+                if (part.HasModuleImplementing<PEAR>())
+                {
+
+                    if (part.GetComponent<PEAR>().isActive) 
+                    {
+                        part.SendEvent("Retract");
+                        part.GetComponent<PEAR>().Events["ExtendAll"].active = true;
+                        part.GetComponent<PEAR>().Events["RetractAll"].active = false;
+
+                    }
+                    
+
+                }
             }
         }
 
         [KSPField(isPersistant = true)]
         public bool gTG = false;
 
+        [KSPField(isPersistant = true)]
+        public bool isActive = true;
+
+        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "PEAR Status:")]
+        public string pearStatus = "Active";
+
         private List<Part> deployableList;
        
-        
+
+           
         // if fairing present, prevent extend action to avoid panels potentially extending through the fairing
         private bool CheckFairing()
         {
@@ -114,13 +162,37 @@ namespace ProgramaticExtensionAndRetraction
                         part.GetComponent<PEAR>().Events["RetractAll"].active = false;
                     }
                 }
+
+               
+
+               
             }
 
             catch
             {
                 return;
             }
+
+
+            
+
         }
+
+        public void SetActiveStatus()
+        {
+            foreach (var part in deployableList)
+            {
+                if (!part.GetComponent<PEAR>().isActive)
+                {
+                    part.GetComponent<PEAR>().pearStatus = "OFFLINE";
+                }
+                else part.GetComponent<PEAR>().pearStatus = "Active";
+            }
+
+
+        }
+
+       
 
         public void Start()
         {
@@ -139,6 +211,7 @@ namespace ProgramaticExtensionAndRetraction
                     }
 
                     CheckState();
+                    SetActiveStatus();
 
                 }
                 catch
@@ -150,6 +223,9 @@ namespace ProgramaticExtensionAndRetraction
 
 
         }
+
+
+       
 
 
     }
